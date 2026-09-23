61e0d98862e30c0ef42200b580f56f7c100ba3bd
tallulahbowden <tallulah.bowden@gmail.com>
2026-09-22 20:34:50 -0400

    Initial commit of Argos Selection Tool script

---
+++ ArgosSelectionTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ArgosSelectionTool.py
+#
+# Description: Reads in an Argos tracking data file and allows
+#   the user to identify the tracked sitings found within a 
+#   specified bounding box.
+#
+# Author: Tallulah Bowden (tsb66@duke.edu)
+# Date:   September 22, 2026
+#--------------------------------------------------------------
