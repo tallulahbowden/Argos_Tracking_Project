510b7b165e7c7d48b57ec204cb8b7cf82628c81d
tallulahbowden <tallulah.bowden@gmail.com>
2026-09-23 13:01:28 -0400

    Process all lines in the argos file using a for loop and print only the ones in the bounding box

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -25,26 +25,28 @@ with open(file_name,'r') as f:
     #Read contents of file into a list
 	line_list = f.readlines()
 
-#Pretend we read one line of data from the file
-lineString = line_list[201]
-
-# Use the split command to parse the items in lineString into a list object
-line_data = lineString.split(',')
-  
-# Assign variables to specfic items in the list
-event_id = line_data[0]   # Argos tracking event ID ("event-id")
-timestamp = line_data[2]  # Observation date ("timestamp")
-lat = float(line_data[4])        # Observation latitude  ("location-lat")
-lon = float(line_data[3])        # Observation longitude ("location-lon")
-lc  = line_data[14]        # Observation location class ("argos:lc")
-tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
-  
-#Evaluate latitude and longitude conditions
-lat_condition = the_box['y_min'] < lat < the_box['y_max']
-lon_condition = the_box['x_min'] < lon < the_box['x_max']
-
-#Report the status of the points
-if lat_condition & lon_condition:
-    print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
-else:
-    print(f'Record {event_id}: {tag_id} was NOT IN the box at {timestamp}')
+#Loop through all the lines
+for lineString in line_list[1:]:
+   
+    # Use the split command to parse the items in lineString into a list object
+    line_data = lineString.split(',')
+       
+    # Assign variables to specfic items in the list
+    event_id = line_data[0]   # Argos tracking event ID ("event-id")
+    timestamp = line_data[2]  # Observation date ("timestamp")
+    lc  = line_data[14]        # Observation location class ("argos:lc")
+    if not lc in ('"1"', '"2"', '"3"'):
+         continue
+    lat = float(line_data[3])        # Observation latitude  ("location-lat")
+    lon = float(line_data[4])        # Observation longitude ("location-lon")
+    tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
+       
+    #Evaluate latitude and longitude conditions
+    lat_condition = the_box['y_min'] < lat < the_box['y_max']
+    lon_condition = the_box['x_min'] < lon < the_box['x_max']
+   
+    #Report the status of the points
+    if lat_condition & lon_condition:
+        print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
+    else:
+         continue
